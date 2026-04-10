d0acf84e516f6b26b973c634b55e9658411a1080
SerhiiHulii <serhii.hulii.92@gmail.com>
2026-04-11 01:28:27 +0200

    upgrade dynamic set opencode configuration

--- scripts/helpers/createOpenCode.js
+++ scripts/helpers/createOpenCode.js
@@ -1,26 +1,17 @@
 import fs from "fs";
+import path from "path";
 import { fileURLToPath } from "url";
 import stripJsonComments from "strip-json-comments";
-import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const filePath = path.join(__dirname, "../config/opencode.example.jsonc");
-
-const raw = fs.readFileSync(filePath, "utf-8");
-const opencode = JSON.parse(stripJsonComments(raw));
-
-// await mkdir('./data', { recursive: true });
-
-// for (const [key, value] of selectedModels) {
-//     await writeFile(`./data/${key}.json`, format(value), null, 2);
-// }
-
-
 export function createOpenCode(parsedProviders) {
+    const exampleFilePath = path.join(__dirname, "../config/opencode.example.jsonc");
+    const raw = fs.readFileSync(exampleFilePath, "utf-8");
+    const opencode = JSON.parse(stripJsonComments(raw));
     opencode.provider = parsedProviders
-    console.log(opencode)
-    writeFile(`./data/${key}.json`, format(value), null, 2);
-    // console.log(parsedProviders)
+
+    const configFilePath = path.join(__dirname, "../../link_data/opencode.json");
+    fs.writeFileSync(filePath, JSON.stringify(opencode, null, 2), "utf-8");
 }
